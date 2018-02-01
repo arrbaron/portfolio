907be06b252e931eeb227b7831acca5ef67421de
Roy Baron <baron.roy@gmail.com>
2018-02-01 00:00:29 -0500

    add flash animation on nav link click

--- scripts/nav.js
+++ scripts/nav.js
@@ -1,6 +1,7 @@
 const Nav = {
   listenersStarted: false,
   scrollSpeed: 500,
+  animationTime: "1.5s",
 
   reset: function() {
     if (!this.listenersStarted) {
@@ -13,18 +14,21 @@ const Nav = {
       $('html, body').animate({
         scrollTop: ($(".projects").offset().top)
       }, this.scrollSpeed);
+      $(".projects__title").transition({animation: "flash", duration: this.animationTime});
     });
 
     $(".link--about").click(() => {
       $('html, body').animate({
         scrollTop: ($(".about").offset().top)
       }, this.scrollSpeed);
+      $(".about__title").transition({animation: "flash", duration: this.animationTime});
     });
 
     $(".link--contact").click(() => {
       $('html, body').animate({
         scrollTop: ($(".contact").offset().top)
       }, this.scrollSpeed);
+      $(".contact__title").transition({animation: "flash", duration: this.animationTime});
     });
   }
 };
